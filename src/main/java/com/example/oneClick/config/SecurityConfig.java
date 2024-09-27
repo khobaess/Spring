c7515fb1d54a32c6091db3f2e6b2c1de576c99b7
Golokova Sofya <khobaess@mail.ru>
2024-09-27 23:46:51 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/oneClick/config/SecurityConfig.java
+++ src/main/java/com/example/oneClick/config/SecurityConfig.java
@@ -38,42 +38,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
-
-    @EnableWebSecurity
-    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-
-        private final PersonDetailsService personDetailsService;
-        @Autowired
-        public SecurityConfig(PersonDetailsService personDetailsService) {
-            this.personDetailsService = personDetailsService;
-        }
-
-
-        @Override
-        protected void configure(HttpSecurity httpSecurity) throws Exception {
-            httpSecurity.authorizeRequests()
-                    .antMatchers("/catalog", "/champagnes", "/energetics", "/lowalcohols",
-                            "/strongalcohols", "/wines", "/champagne/**",  "/energetic/**",
-                            "/lowalcohol/**",  "/strongalcohol/**",  "/wine/**", "/cart",
-                            "/auth/registration", "/product_edit", "/product_edit/**}",
-                            "/champagne_edit/", "/champagne_edit/").permitAll()
-                    .and()
-                    .formLogin().loginPage("/auth/login")
-                    .loginProcessingUrl("/process_login")
-                    .defaultSuccessUrl("/catalog")
-                    .failureUrl("/auth/login?error")
-                    .and()
-                    .logout()
-                    .logoutUrl("/logout")
-                    .logoutSuccessUrl("/auth/login");
-
-
-        }
-
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.userDetailsService(personDetailsService);
-        }
-    }
 }
